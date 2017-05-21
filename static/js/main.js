3c26b29fcd9d5ebad1cd044447181cd2be8f8dfa
Frozen Helium <fren.ankit@gmail.com>
2017-05-21 16:15:44 +0545

    Add timeline in country profile

--- static/js/main.js
+++ static/js/main.js
@@ -698,6 +698,23 @@ class BarChart extends Element {
 class Timeline extends Element {
     constructor() {
         super('<div id="timeline-container"></div>')
+        this.timelineElementTemplate = $('<div class="timeline-element"><div class="number"><span></span></div><div class="description"><h5></h5><p></p></div><img></div>');
+    }
+
+    getTimelineElement(num, title, descText, imgUri) {
+        let elem = this.timelineElementTemplate.clone();
+        elem.find('.number > span').text(num);
+        elem.find('h5').text(title);
+        elem.find('p').text(descText);
+        elem.find('img').prop('src', imgUri);
+        return elem;
+    }
+
+    process() {
+        this.getTimelineElement("01", "Hello World", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut",  pluginDir +'/static/img/chart2.png').appendTo(this.element);
+        this.getTimelineElement("02", "Item 2", "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",  pluginDir +'/static/img/chart1.png').appendTo(this.element);
+        this.getTimelineElement("03", "Item 3", "dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",  pluginDir +'/static/img/chart3.png').appendTo(this.element);
+
     }
 }
 
@@ -764,7 +781,7 @@ $(document).ready(function(){
     $.ajax({
         method: 'GET',
         url: 'https://api.acleddata.com/acled/read.csv',
-        data: {'limit': '10000', 'fields': 'country|event_type|latitude|longitude|fatalities'},
+        data: {'limit': '1000', 'fields': 'country|event_type|latitude|longitude|fatalities'},
         success: function(data){
             let rows = data.split('\n');
             let keys = rows[0].split(',');
