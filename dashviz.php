8767ab8713e7600ec3573f0ca19f04720b496df9
frozenhelium <fren.ankit@gmail.com>
2018-02-03 18:13:36 +0545

    Increase timestamp to avoid cache

--- dashviz.php
+++ dashviz.php
@@ -9,8 +9,8 @@ Version: 1.0
 
 define( 'DASHVIZ__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
-$adminModifiedTimestamp='360344047';
-$clientModifiedTimestamp='360444047';
+$adminModifiedTimestamp='360344057';
+$clientModifiedTimestamp='360444057';
 
 
 class DashboardVisualization {
